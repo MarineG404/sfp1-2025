e54fc6dbf95d652b45235852211ce3a177c0d31a
clement kieu <c.kieu@lyon.ort.asso.fr>
2025-10-16 11:56:12 +0200

    mise du css

--- public/developers/clement-kieu.php
+++ public/developers/clement-kieu.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clémant KIEU</title>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../assets/css/clement.css">
+	<title>Clémant KIEU</title>
 </head>
+
 <body>
-    <h1>Clémant KIEU</h1>
-    <p>Date/Heure actuelle : <?php echo date('Y-m-d H:i:s'); ?></p>
-    <h2>Résumé du CV</h2>
-    <p>>Je suis une personne rigoureux et investi qui saurai déployer l’énergie et la capacité de travail nécessaires afin de réussir les missions confiées pour le développement de l’entreprise.</p>
-	<a href="../phpinfo.php"></a>
+	<h1>Clémant KIEU</h1>
+	<p>Date/Heure actuelle : <?php echo date('Y-m-d H:i:s'); ?></p>
+	<h2>Résumé du CV</h2>
+	<p>>Je suis une personne rigoureux et investi qui saurai déployer l’énergie et la capacité de travail nécessaires afin de réussir les missions confiées pour le développement de l’entreprise.</p>
+	<a href="../index.php">Acceuil</a>
 </body>
+
 </html>
