b664a2ddd78761a52407958a4aaa3ca3a107bcea
clement kieu <c.kieu@lyon.ort.asso.fr>
2025-10-16 11:05:52 +0200

    création de ma page

--- public/developers/clement-kieu.php
+++ public/developers/clement-kieu.php
@@ -9,6 +9,7 @@
     <h1>Clémant KIEU</h1>
     <p>Date/Heure actuelle : <?php echo date('Y-m-d H:i:s'); ?></p>
     <h2>Résumé du CV</h2>
-    <p>Clémant KIEU est un développeur passionné par les technologies web. Il a une expérience dans le développement PHP et JavaScript, et il aime travailler sur des projets innovants.</p>
+    <p>>Je suis une personne rigoureux et investi qui saurai déployer l’énergie et la capacité de travail nécessaires afin de réussir les missions confiées pour le développement de l’entreprise.</p>
+
 </body>
 </html>
