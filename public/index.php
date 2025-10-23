6d9cd5fe99b30233eb6601841195ed3b414dbb21
MarineG404 <marine.gonnord.pro@gmail.com>
2025-10-23 10:46:20 +0200

    fix affichage branche

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '1.0.0');
+define('APP_VERSION', '1.1.1');
 
 // Main entry point of the application
 
@@ -7,7 +7,31 @@ define('APP_VERSION', '1.0.0');
 include_once __DIR__ . '/../includes/version.php';
 
 // Get the current branch name from the environment variable or set a default
-$branch = getenv('GIT_BRANCH') ?: 'dev';
+function findGitHeadFile($dir) {
+    while ($dir !== '/' && $dir !== '') {
+        $gitHead = $dir . '/.git/HEAD';
+        if (file_exists($gitHead)) {
+            return $gitHead;
+        }
+        $dir = dirname($dir);
+    }
+    return null;
+}
+
+$headFile = findGitHeadFile(__DIR__);
+
+if ($headFile) {
+    $content = trim(file_get_contents($headFile));
+    if (strpos($content, 'ref:') === 0) {
+        $branch = basename(trim(substr($content, 5)));
+    } else {
+        $branch = substr($content, 0, 7); // commit détaché
+    }
+} else {
+    $branch = 'ouin ouin';
+}
+
+
 $phpVersion = phpversion();
 $currentDateTime = date('Y-m-d H:i:s');
 ?>
