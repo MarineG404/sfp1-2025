30b32ce8f5e883699063d204069cff375e987dc8
clement kieu <c.kieu@lyon.ort.asso.fr>
2025-11-06 14:15:33 +0100

    début du scripting

--- public/developers/clement-kieu.php
+++ public/developers/clement-kieu.php
@@ -15,6 +15,7 @@
 	<h2>Résumé du CV</h2>
 	<p>>Je suis une personne rigoureux et investi qui saurai déployer l’énergie et la capacité de travail nécessaires afin de réussir les missions confiées pour le développement de l’entreprise.</p>
 	<a href="../index.php">Acceuil</a>
+	<button onclick="pull()">pull</button>
 </body>
 
 </html>
@@ -22,4 +23,11 @@
 <?php
 	$resultatcommande = shell_exec('git branch --show-current');
 	echo "<p>$resultatcommande<p>";
+
+	function pull()
+	{
+
+	}
+	/*$pull = shell_exec("git pull");*/
+
 ?>
