23dfce5b41e1c271fcb4951566ab5be2975b0b0c
clement kieu <c.kieu@lyon.ort.asso.fr>
2025-11-06 14:17:05 +0100

    début + dossier

---
+++ clement-script.php
@@ -0,0 +1,13 @@
+<?php
+# le but du script est d'ajouter des photos dans un dossier 
+
+file_put_contents('../photo-site/clementphoto.jpeg','c est une image')
+
+
+
+# ajout dans les bases de données 
+
+
+
+
+?>
