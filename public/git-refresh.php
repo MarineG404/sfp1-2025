c5c53ff42ef1a25923dd228ec9616d5ba163e2ed
MarineG404 <marine.gonnord.pro@gmail.com>
2025-11-13 11:30:14 +0100

    cc

--- public/git-refresh.php
+++ public/git-refresh.php
@@ -14,6 +14,8 @@ if ($exitCode !== 0) {
 	echo 'Mise à jour Git réussie.<br><pre>' . htmlspecialchars(implode("\n", $output)) . '</pre>';
 }
 
+echo "coucou "
+
 // header('Location: index.php');
-exit;
+// exit;
 ?>
