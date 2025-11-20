7464c3691fc7c4ee1ab71e4c3918450acbb6e6bd
MarineG404 <marine.gonnord.pro@gmail.com>
2025-11-20 16:18:59 +0100

    Update .gitignore and clean up git-refresh.php and index.php

--- public/git-refresh.php
+++ public/git-refresh.php
@@ -12,9 +12,9 @@ if ($exitCode !== 0) {
 	die('Échec de la mise à jour Git. Code de sortie : ' . $exitCode);
 } else {
 	echo 'Mise à jour Git réussie.<br><pre>' . htmlspecialchars(implode("\n", $output)) . '</pre>';
+	header('Location: index.php');
 }
 
 
-header('Location: index.php');
 exit;
 ?>
